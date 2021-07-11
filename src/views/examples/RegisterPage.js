5919d616a18d6f916c141913feb13ff5b16ac3f0
VikTheTrick <viktorvujicic1@gmail.com>
2021-07-12 00:24:35 +0200

    sssss

--- src/views/examples/RegisterPage.js
+++ src/views/examples/RegisterPage.js
@@ -15,9 +15,51 @@ import ReactDatetime from "react-datetime";
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 
 function RegisterPage(){
+  
+ 
   document.documentElement.classList.remove("nav-open");
   
   const [modal, setModal] = React.useState(false);
+  const[link,setLink] = React.useState("");
+  const[naziv,setNaziv] = React.useState("");
+  const[pocetna,Setpocetna] = React.useState(0);
+  const[opis,Setopis] = React.useState("");
+  const[stanje,SetStanje] = React.useState(false);
+  const[niza,SetNiza] = React.useState([]);
+let niz=[];
+let pret=0;
+let sad=1;
+let words = [];
+const[i,SetI]=React.useState(0);
+ const onpres=()=>
+ {
+   sad+=1;
+   if(pret==sad)
+   { 
+     sad+=1;
+   console.log("desilo se")
+   if(link!==""&&naziv!=="")
+   { 
+     console.log("sssss")
+     words.pop();
+     let a={ime:naziv,prezime:i}
+     words.push(a);
+     console.log(niza)
+     SetNiza([...niza,{ime:naziv,prezime:pocetna}])
+   niz.push({naziv:naziv,link:link,pocetna:pocetna,opis:opis,id:i});
+   
+   SetI(i+1);
+
+   }
+   else
+   {
+     console.log("error");
+     console.log(niz);
+   
+  }
+}
+  
+ }
   const toggleModal = () => {
     setModal(!modal);
   };
@@ -36,23 +78,23 @@ function RegisterPage(){
         className="page-header"
         style={{
           backgroundImage:
-            "url(" + require("assets/img/login-image.jpg").default + ")",
+            "url(" + require("assets/img/header.jpg").default + ")",
         }}
       >
         <div className="filter" />
-        <Container>
-          <Row>
+        <Container style={{backgroundColor:'black',borderRadius:10,height:'160vh',overflowY:'scroll'}}>
+          <Row  style={{flexDirection:'row'}}>
             <Col className="ml-auto mr-auto" lg="4">
               <Card className="card-register ml-auto mr-auto">
                 <h3 className="title mx-auto"><b>Create your NFT</b></h3>
 
                 <Form className="register-form">
                   <label><h6>NFT Name *</h6></label>
-                  <Input placeholder="Enter your NFT name here" type="text" />
+                  <Input onChange={(value)=>{setNaziv(value)}} placeholder="Enter your NFT name here" type="text" />
                   <label><h6>URL *</h6></label>
-                  <Input placeholder="Enter your NFT url here" type="text" />
+                  <Input onChange={(value)=>{setLink(value)}} placeholder="Enter your NFT url here" type="text" />
                   <label><h6>Starting Price *</h6></label>
-                  <Input placeholder="Enter starting price" type="number" />
+                  <Input onChange={(value)=>{Setpocetna(value)}} placeholder="Enter starting price" type="number" />
                   <label><h6>Pick Date *</h6></label>
                   <div color='danger'>
                     <InputGroup className="date" id="datetimepicker">
@@ -70,9 +112,9 @@ function RegisterPage(){
                       </InputGroupAddon>
                     </InputGroup>
                     <label><h6>Description</h6></label>
-                  <Input placeholder="(optional)" type="text" />
+                  <Input  onChange={(value)=>{Setopis(value)}} placeholder="(optional)" type="text" />
                   <center>
-                    <Button className="btn-round" color="danger" href="/register-page">
+                    <Button disabled={(link!==""&&naziv!=="")?false:true} onClick={()=>{onpres()}} className="btn-round" color="danger" href="/register-page">
                       Create
                     </Button>
                   </center>
@@ -81,97 +123,33 @@ function RegisterPage(){
                 </Form>
               </Card>
             </Col>
-            <Col className="ml-auto mr-auto" lg="8">
-              <Card className="card-profile">
-                <h3 className="title mx-auto"><b>Browse existing NFT's</b></h3>
-              
-              <Row>
-              <Col>
-              <div class="col-sm-6 col-md-10" >
-                <img 
-                  alt="..." 
-                  class="img-rounded img-responsive" 
-                  src="/paper-kit-react/static/media/uriel-soberanes.b741f54e.jpg"
-                  onClick={toggleModal}
-                  />
-                  <Modal isOpen={modal} toggle={toggleModal}>
-                    {/* MODAL ZA PRVU SLIKU */}
-                    <div className="modal-header">
-                      <button
-                        aria-label="Close"
-                        className="close"
-                        type="button"
-                        onClick={toggleModal}
-                      >
-                        <span aria-hidden={true}>×</span>
-                      </button>
-                      <h5
-                        className="modal-title text-center"
-                        id="exampleModalLabel"
-                      >
-                        Picture Name
-                      </h5>
-                    </div>
-                    <div className="modal-body">
-                        <img 
-                      alt="..." 
-                      class="img-rounded img-responsive" 
-                      src="/paper-kit-react/static/media/uriel-soberanes.b741f54e.jpg"
-                      />
-                    </div>
-                    <div className="modal-footer">
-                      <div className="left-side">
-                        <Button
-                          className="btn-link"
-                          color="default"
-                          type="button"
-                        >
-                          Current NFT Value: 0.015 ETH
-                        </Button>
-                      </div>
-                      <div className="divider" />
-                      <div className="right-side">
-                        <Button className="btn-link" color="danger" type="button">
-                          Bid:
-                        <Input placeholder="Enter starting price" type="number" />
-                        </Button>
-                      </div>
-                    </div>
-                  </Modal>
-                  
-                  <p className="text-center"><b>Current Value: 0.015 ETH</b></p>
-              </div>
-              </Col>
-              
-              <Col>
-              <div class="col-sm-6 col-md-10">
+            <Col  className="ml-auto mr-auto" lg="8">
+              <Card  className="card-profile" style={{ backgroundColor:'#f5f5f5',flexDirection:'column',marginBottom:100,height:1000,overflowY:'scroll',justifyContent:'center',alignItems:'center',paddingLeft:100 }}>
+                <h3 className="title mx-auto" style={{marginRight:100}}><b>Browse existing NFT's</b></h3>
+                <ul>
+                
+                    <div class="col-sm-6 col-md-10">
                 <img 
                   alt="..." 
                   class="img-rounded img-responsive" 
                   src="/paper-kit-react/static/media/uriel-soberanes.b741f54e.jpg"/>
                   <p className="text-center"><b>Current Value: 1.400 ETH</b></p>
-              </div> 
-              </Col>
-
-              <Col>            
-              <div class="col-sm-6 col-md-10">
-                <img 
-                  alt="..." 
-                  class="img-rounded img-responsive" 
-                  src="/paper-kit-react/static/media/uriel-soberanes.b741f54e.jpg"/>
-                  <p className="text-center"><b>Current Value: 0.523 ETH</b></p>
               </div>
-              </Col> 
-              </Row>
+               
+
+ 
+ </ul>
+             
+              
               
               </Card>
             </Col>
           </Row>
         </Container>
         <div className="footer register-footer text-center">
-          <h6>
+          <h6 style={{color:'black'}}>
             {new Date().getFullYear()}, made with{" "}
-            <i className="fa fa-heart heart" /> by Dogecoin crew
+            <i className="fa fa-heart heart" style={{color:'red'}} /> by Dogecoin crew
           </h6>
         </div>
       </div>
